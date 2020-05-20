3b28c34f89f2b35c1be3482ed38b8f309ebd2a96
KhanyisoCairo <Khanyisoyezo12@gmail.com>
2020-05-20 13:57:21 +0200

    added description on clases

--- src/vending/SaltySnacks.java
+++ src/vending/SaltySnacks.java
@@ -1,5 +1,7 @@
 package vending;
 
 public class SaltySnacks extends Products{
-
+public String description(){
+    return "This are Snacks";
+}
 }
--- src/vending/SoftDrinks.java
+++ src/vending/SoftDrinks.java
@@ -1,5 +1,7 @@
 package vending;
 
 public class SoftDrinks extends Products{
-
+public String description(){
+    return "This is a Softdrink";
+}
 }
